16690c8b0c4f3c54aabd02c3e2b7395940963efc
wmr123456 <844261654@qq.com>
2016-07-04 15:21:45 +0800

    删除重复代码，添加新类型。

---
+++ src/com/idrilling/designpatterns/charptereight/NvWa.java
@@ -0,0 +1,10 @@
+package com.idrilling.designpatterns.charptereight;
+
+/**
+ * Created by mrwang on 16-7-4.
+ */
+public class NvWa {
+    public static void main(String[] args){
+        
+    }
+}
